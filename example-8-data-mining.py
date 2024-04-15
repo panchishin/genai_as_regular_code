c415f1a4d0a542ba203e643d1e70537261939d59
Anton Panchishin <anton.panchishin@uk.ey.com>
2024-04-15 12:43:19 -0400

    Improve extract example to get multiple points of data

--- example-8-data-mining.py
+++ example-8-data-mining.py
@@ -4,27 +4,38 @@ import json
 print("""
 All the previous examples covered classification or correction tasks.
 In this example we will extract data from a text.
-Specifically we will extract the subject of a sentence.
+Specifically we will extract the subject, verb, and object of a sentence.
 
-In this example we use a 2 examples in our prompt to help the LLM understand the task.
+We will use JSON to help the LLM produce easily parseable results.
 """)
 
-parse_subject = LLM(prompt="""Reply to my following message with just the subject of the sentence.
-For example, if my message is 'When Billy went to the store he remembered to buy cheese' then your reply will be 'Billy'.
-For example, if my message is 'LLMs are a great data mining tool' then your reply will be 'LLMs'.
-Only use the information in my following message to determine the subject.""")
+
+parse_subject = LLM(prompt="""Reply to my following message with the following json data:
+{
+    "subject": ... ,
+    "verb": ... ,
+    "object": ...
+}""", format="json")
+
 
 data_to_parse = (
     'According to the last messages from Stephanie, there is much needed attention on the braking system of the DRF',
     'On Tuesday, Jamie went to the store to buy some groceries',
     'Of all the people in London, only Greggory has never heard of the Queen',
     'UTTT is a game that is played on a 9x9 grid',
+    'I am',
+    'In the middle of the night, on a strange island, in a cabin made of honey, a bear woke up',
 )
+
 
 for sentence in data_to_parse:
     print("")
     print("Sentence :", sentence)
-    print("Subject :", parse_subject.process(data=sentence))
+    result = json.loads(parse_subject.process(data=sentence))
+    print("Subject :", result["subject"])
+    print("Verb :", result["verb"])
+    print("Object :", result["object"])
+
 
 print("""
 Done!
